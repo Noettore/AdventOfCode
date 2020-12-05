a7dee4f9db31889a11e650a2bdfa070bf16aea1d
Ettore Dreucci <ettore.dreucci@gmail.com>
2020-12-05 15:23:08 +0100

    AoC 2020: day5: power of two multiplication
    Signed-off-by: Ettore Dreucci <ettore.dreucci@gmail.com>

--- 2020-python/solutions/day_5.py
+++ 2020-python/solutions/day_5.py
@@ -10,14 +10,10 @@ def read_input(input_path: str) -> list:
         seats = list()
         for entry in entries:
             entry = entry.strip()
-            row = ''.join(['0' if letter == 'F' else '1' for letter in entry[:7]])
-            column = ''.join(['0' if letter == 'L' else '1' for letter in entry[-3:]])
-            seat = int(row, 2) * 8 + int(column, 2)
+            seat = int(''.join(['0' if letter in 'FL' else '1' for letter in entry]), 2)
             seats.append(seat)
         seats.sort()
         return seats
-
-
 
 def part1(entries: list) -> int:
     """part1 solver take a list of strings and return an int"""
@@ -38,7 +34,6 @@ def main():
     print("Part 2: %d" % part2(entries))
     end_time = time.time()
     print("Execution time: %f" % (end_time-start_time))
-
 
 if __name__ == "__main__":
     main()
